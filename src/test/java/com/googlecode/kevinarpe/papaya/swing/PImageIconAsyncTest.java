6ea76806b07160b22237a325bb36ffad7789dafd
Kevin Connor ARPE <kevinarpe@gmail.com>
2013-08-04 02:55:31 +0800

    Added licenses

--- src/test/java/com/googlecode/kevinarpe/papaya/swing/PImageIconAsyncTest.java
+++ src/test/java/com/googlecode/kevinarpe/papaya/swing/PImageIconAsyncTest.java
@@ -1,5 +1,30 @@
 package com.googlecode.kevinarpe.papaya.swing;
 
+/*
+ * #%L
+ * This file is part of Papaya Swing.
+ * %%
+ * Copyright (C) 2013 Kevin Connor ARPE (kevinarpe@gmail.com)
+ * %%
+ * Papaya Swing is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * GPL Classpath Exception:
+ * This project is subject to the "Classpath" exception as provided in
+ * the LICENSE file that accompanied this code.
+ * 
+ * Papaya Swing is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Papaya Swing.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.io.File;
