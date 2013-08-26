05722de4464ee69f32bd62ed0667a85e3f2a6af9
Kevin Connor ARPE <kevinarpe@gmail.com>
2013-08-27 00:33:25 +0800

    Checkpoint
    * Improved PArgbPixelMask -- new static methods and data members
    * Trying to write a few versions of PixelOp for grayscale

--- src/main/java/com/googlecode/kevinarpe/papaya/swing/image/PArgbBufferedImageToGrayscaleOp.java
+++ src/main/java/com/googlecode/kevinarpe/papaya/swing/image/PArgbBufferedImageToGrayscaleOp.java
@@ -25,6 +25,7 @@ package com.googlecode.kevinarpe.papaya.swing.image;
  * #L%
  */
 
+import java.awt.color.ColorSpace;
 import java.awt.image.ColorModel;
 
 import com.googlecode.kevinarpe.papaya.annotation.NotFullyTested;
@@ -55,7 +56,7 @@ extends PAbstractArgbBufferedImageOp {
      * Ref: <a href="http://en.wikipedia.org/wiki/YUV#BT.709_and_BT.601">
      * http://en.wikipedia.org/wiki/YUV#BT.709_and_BT.601</a>
      */
-    public final float BT_709_LUMA_COEFFICIENT_RED = 0.2126f;
+    public static final float BT_709_LUMA_COEFFICIENT_RED = 0.2126f;
 
     /**
      * Luma coefficient from HDTV standard for green component to grayscale conversion: {@code 0.7152f}
@@ -63,7 +64,7 @@ extends PAbstractArgbBufferedImageOp {
      * Ref: <a href="http://en.wikipedia.org/wiki/YUV#BT.709_and_BT.601">
      * http://en.wikipedia.org/wiki/YUV#BT.709_and_BT.601</a>
      */
-    public final float BT_709_LUMA_COEFFICIENT_GREEN = 0.7152f;
+    public static final float BT_709_LUMA_COEFFICIENT_GREEN = 0.7152f;
     
     /**
      * Luma coefficient from HDTV standard for blue component to grayscale conversion: {@code 0.0722f}
@@ -71,10 +72,20 @@ extends PAbstractArgbBufferedImageOp {
      * Ref: <a href="http://en.wikipedia.org/wiki/YUV#BT.709_and_BT.601">
      * http://en.wikipedia.org/wiki/YUV#BT.709_and_BT.601</a>
      */
-    public final float BT_709_LUMA_COEFFICIENT_BLUE = 0.0722f;
+    public static final float BT_709_LUMA_COEFFICIENT_BLUE = 0.0722f;
     
     public PArgbBufferedImageToGrayscaleOp() {
         super();
+    }
+    
+    private boolean _useLinearGammaDecompressedColors = false;
+    
+    public boolean useLinearGammaDecompressedColors() {
+        return _useLinearGammaDecompressedColors;
+    }
+    
+    public void useLinearGammaDecompressedColors(boolean x) {
+        _useLinearGammaDecompressedColors = x;
     }
 
     @Override
@@ -110,6 +121,98 @@ extends PAbstractArgbBufferedImageOp {
             final int red255b = (PArgbPixelMask.RED.mask & pixel2) >> PArgbPixelMask.RED.bitShift;
             final int green255b = (PArgbPixelMask.GREEN.mask & pixel2) >> PArgbPixelMask.GREEN.bitShift;
             final int blue255b = (PArgbPixelMask.BLUE.mask & pixel2) >> PArgbPixelMask.BLUE.bitShift;
+                
+            pixelArr[i] = pixel2;
+        }
+    }
+    
+    //@Override
+    protected void processPixels2(int[] pixelArr, ColorModel colorModel) {
+        final int size = pixelArr.length;
+        final int[] argbComponent4Arr = new int[4];
+        for (int i = 0; i < size; ++i) {
+            final int pixel = pixelArr[i];
+            PArgbPixelMask.decomposeArgbPixelUnchecked(pixel, argbComponent4Arr);
+            
+            final int grayscale255 = Math.round(
+                (BT_709_LUMA_COEFFICIENT_RED
+                    * argbComponent4Arr[PArgbPixelMask.RED.component4ArrayIndex])
+                + (BT_709_LUMA_COEFFICIENT_GREEN
+                    * argbComponent4Arr[PArgbPixelMask.GREEN.component4ArrayIndex])
+                + (BT_709_LUMA_COEFFICIENT_BLUE
+                    * argbComponent4Arr[PArgbPixelMask.BLUE.component4ArrayIndex]));
+//            if (grayscale255 > 255) {
+//                grayscale255 = 255;
+//            }
+            
+            for (PArgbPixelMask pixelMask: PArgbPixelMask.values()) {
+                if (PArgbPixelMask.ALPHA != pixelMask) {
+                    argbComponent4Arr[pixelMask.component4ArrayIndex] = grayscale255;
+                }
+            }
+            
+            final int pixel2 = PArgbPixelMask.composeArgbPixelUnchecked(argbComponent4Arr);
+            
+            // For the debugger!
+//            final int alpha255b = (PArgbPixelMask.ALPHA.mask & pixel2) >> PArgbPixelMask.ALPHA.bitShift;
+//            final int red255b = (PArgbPixelMask.RED.mask & pixel2) >> PArgbPixelMask.RED.bitShift;
+//            final int green255b = (PArgbPixelMask.GREEN.mask & pixel2) >> PArgbPixelMask.GREEN.bitShift;
+//            final int blue255b = (PArgbPixelMask.BLUE.mask & pixel2) >> PArgbPixelMask.BLUE.bitShift;
+                
+            pixelArr[i] = pixel2;
+        }
+    }
+    
+    //@Override
+    protected void processPixels3(int[] pixelArr, ColorModel colorModel) {
+        final int size = pixelArr.length;
+        final float[] argbComponent4Arr = new float[4];
+        final float[] nonLinearRgbComponent3Arr = new float[3];
+        final ColorSpace linearRgbColorSpace = ColorSpace.getInstance(ColorSpace.CS_LINEAR_RGB);
+        for (int i = 0; i < size; ++i) {
+            final int pixel = pixelArr[i];
+            PArgbPixelMask.decomposeArgbPixelUnchecked(pixel, argbComponent4Arr);
+            
+            nonLinearRgbComponent3Arr[PArgbPixelMask.RED.component3ArrayIndex] =
+                argbComponent4Arr[PArgbPixelMask.RED.component4ArrayIndex];
+            
+            nonLinearRgbComponent3Arr[PArgbPixelMask.GREEN.component3ArrayIndex] =
+                argbComponent4Arr[PArgbPixelMask.GREEN.component4ArrayIndex];
+            
+            nonLinearRgbComponent3Arr[PArgbPixelMask.BLUE.component3ArrayIndex] =
+                argbComponent4Arr[PArgbPixelMask.BLUE.component4ArrayIndex];
+            
+            final float[] linearRgbComponent3Arr =
+                linearRgbColorSpace.fromRGB(nonLinearRgbComponent3Arr);
+            
+            final float grayscale255 =
+                (BT_709_LUMA_COEFFICIENT_RED * linearRgbComponent3Arr[0])
+                + (BT_709_LUMA_COEFFICIENT_GREEN * linearRgbComponent3Arr[1])
+                + (BT_709_LUMA_COEFFICIENT_BLUE * linearRgbComponent3Arr[2]);
+            
+            linearRgbComponent3Arr[0] = grayscale255;
+            linearRgbComponent3Arr[1] = grayscale255;
+            linearRgbComponent3Arr[2] = grayscale255;
+            
+            final float[] nonLinearRgbComponent3GrayscaleArr =
+                linearRgbColorSpace.toRGB(linearRgbComponent3Arr);
+            
+            argbComponent4Arr[PArgbPixelMask.RED.component4ArrayIndex] =
+                nonLinearRgbComponent3GrayscaleArr[PArgbPixelMask.RED.component3ArrayIndex];
+            
+            argbComponent4Arr[PArgbPixelMask.GREEN.component4ArrayIndex] =
+                nonLinearRgbComponent3GrayscaleArr[PArgbPixelMask.GREEN.component3ArrayIndex];
+            
+            argbComponent4Arr[PArgbPixelMask.BLUE.component4ArrayIndex] =
+                nonLinearRgbComponent3GrayscaleArr[PArgbPixelMask.BLUE.component3ArrayIndex];
+            
+            final int pixel2 = PArgbPixelMask.composeArgbPixelUnchecked(argbComponent4Arr);
+            
+            // For the debugger!
+//            final int alpha255b = (PArgbPixelMask.ALPHA.mask & pixel2) >> PArgbPixelMask.ALPHA.bitShift;
+//            final int red255b = (PArgbPixelMask.RED.mask & pixel2) >> PArgbPixelMask.RED.bitShift;
+//            final int green255b = (PArgbPixelMask.GREEN.mask & pixel2) >> PArgbPixelMask.GREEN.bitShift;
+//            final int blue255b = (PArgbPixelMask.BLUE.mask & pixel2) >> PArgbPixelMask.BLUE.bitShift;
                 
             pixelArr[i] = pixel2;
         }
--- src/main/java/com/googlecode/kevinarpe/papaya/swing/image/PArgbPixelMask.java
+++ src/main/java/com/googlecode/kevinarpe/papaya/swing/image/PArgbPixelMask.java
@@ -26,6 +26,8 @@ package com.googlecode.kevinarpe.papaya.swing.image;
  */
 
 import com.googlecode.kevinarpe.papaya.annotation.NotFullyTested;
+import com.googlecode.kevinarpe.papaya.argument.ArrayArgs;
+import com.googlecode.kevinarpe.papaya.argument.IntArgs;
 
 /**
  * Collection of bit masks to extract channels from an integer-packed ARGB pixel.  These masks also
@@ -33,10 +35,10 @@ import com.googlecode.kevinarpe.papaya.annotation.NotFullyTested;
  * <p>
  * Example: <pre>{@code
  * int pixel = ...;
- * int alpha = pixel & PixelMask.ALPHA.mask;
- * int red = pixel & PixelMask.RED.mask;
- * int green = pixel & PixelMask.GREEN.mask;
- * int blue = pixel & PixelMask.BLUE.mask;
+ * int alpha255 = (pixel & PixelMask.ALPHA.mask) >> PArgbPixelMask.ALPHA.bitShift;
+ * int red255 = (pixel & PixelMask.RED.mask) >> PArgbPixelMask.RED.bitShift;
+ * int green255 = (pixel & PixelMask.GREEN.mask) >> PArgbPixelMask.GREEN.bitShift;
+ * int blue255 = (pixel & PixelMask.BLUE.mask) >> PArgbPixelMask.BLUE.bitShift;
  * }</pre>
  * 
  * @author Kevin Connor ARPE (kevinarpe@gmail.com)
@@ -48,73 +50,289 @@ public enum PArgbPixelMask {
      * Bit mask to extract alpha channel from an integer-packed ARGB pixel.  If a pixel is strictly
      * RGB, the alpha channel will be zero.
      * <p>
-     * {@link #bitShift} is 24, {@link #mask} is 0xff000000.
+     * <ul>
+     *   <li>{@link #bitShift} is 24</li>
+     *   <li>{@link #mask} is 0xff000000</li>
+     *   <li>{@link #component4ArrayIndex} is 0</li>
+     *   <li>{@link #component3ArrayIndex} is -1 (invalid)</li>
+     * </ul>
      * <p>
-     * Example: <pre>{@code
-     * int pixel = ...;
-     * int alpha = pixel & PixelMask.ALPHA.mask;
-     * }</pre>
+     * Example:
+     * {@code int alpha255 = (pixel & PixelMask.ALPHA.mask) >> PixelMask.ALPHA.bitShift;}
      */
-    ALPHA(24),
+    ALPHA(24, 0),
     
     /**
      * Bit mask to extract red color channel from an integer-packed ARGB (or strictly RGB) pixel.
      * <p>
-     * {@link #bitShift} is 16, {@link #mask} is 0x00ff0000.
+     * <ul>
+     *   <li>{@link #bitShift} is 16</li>
+     *   <li>{@link #mask} is 0x00ff0000</li>
+     *   <li>{@link #component4ArrayIndex} is 1</li>
+     *   <li>{@link #component3ArrayIndex} is 0</li>
+     * </ul>
      * <p>
-     * Example: <pre>{@code
-     * int pixel = ...;
-     * int red = pixel & PixelMask.RED.mask;
-     * }</pre>
+     * Example:
+     * {@code int red255 = (pixel & PixelMask.RED.mask) >> PixelMask.RED.bitShift;}
      */
-    RED  (16),
+    RED(16, 1),
     
     /**
      * Bit mask to extract green color channel from an integer-packed ARGB (or strictly RGB) pixel.
      * <p>
-     * {@link #bitShift} is 8, {@link #mask} is 0x0000ff00.
+     * <ul>
+     *   <li>{@link #bitShift} is 8</li>
+     *   <li>{@link #mask} is 0x0000ff00</li>
+     *   <li>{@link #component4ArrayIndex} is 2</li>
+     *   <li>{@link #component3ArrayIndex} is 1</li>
+     * </ul>
      * <p>
-     * Example: <pre>{@code
-     * int pixel = ...;
-     * int green = pixel & PixelMask.GREEN.mask;
-     * }</pre>
+     * Example:
+     * {@code int green255 = (pixel & PixelMask.GREEN.mask) >> PixelMask.GREEN.bitShift;}
      */
-    GREEN(8),
+    GREEN(8, 2),
     
     /**
      * Bit mask to extract blue color channel from an integer-packed ARGB (or strictly RGB) pixel.
      * <p>
-     * {@link #bitShift} is 0, {@link #mask} is 0x000000ff
+     * <ul>
+     *   <li>{@link #bitShift} is 0</li>
+     *   <li>{@link #mask} is 0x000000ff</li>
+     *   <li>{@link #component4ArrayIndex} is 3</li>
+     *   <li>{@link #component3ArrayIndex} is 2</li>
+     * </ul>
      * <p>
-     * Example: <pre>{@code
-     * int pixel = ...;
-     * int blue = pixel & PixelMask.BLUE.mask;
-     * }</pre>
+     * Example:
+     * {@code int blue255 = (pixel & PixelMask.BLUE.mask) >> PixelMask.BLUE.bitShift;}
      */
-    BLUE (0),
+    BLUE(0, 3),
     ;
+
+    /**
+     * Mask to extract a channel from an integer-package ARGB (or strictly RGB) pixel.  If
+     * necessary, shift the resulting bits with {@link #bitShift}.
+     * <p>
+     * Example: {@code pixel & PixelMask.BLUE.mask}
+     */
+    public final int mask;
     
     /**
      * Number of bits to shift to access a single channel.
      * <p>
      * Example: <pre>{@code 
-     * int pixel = alpha << PArgbPixelMask.ALPHA.bitShift
-     *           & red << PArgbPixelMask.RED.bitShift
-     *           & green << PArgbPixelMask.GREEN.bitShift
-     *           & blue << PArgbPixelMask.BLUE.bitShift;
+     * int pixel = alpha255 << PArgbPixelMask.ALPHA.bitShift
+     *           | red255 << PArgbPixelMask.RED.bitShift
+     *           | green255 << PArgbPixelMask.GREEN.bitShift
+     *           | blue255 << PArgbPixelMask.BLUE.bitShift;
      * }</pre>
      * <p>
      * Hint: Order does not matter in the example above.
      */
     public final int bitShift;
-
-    /**
-     * Mask to extract a channel from an integer-package ARGB (or strictly RGB) pixel.
-     */
-    public final int mask;
     
-    private PArgbPixelMask(int bitShift) {
-        this.bitShift = bitShift;
+    /**
+     * Index for ARGB (4-part) component arrays.  Used by (de)compose helper methods, e.g.,
+     * {@link #decomposeArgbPixelUnchecked(int, int[])} and
+     * {@link #composeArgbPixelChecked(float[])}.
+     * <p>
+     * Example: <pre>{@code
+     * int[] argbComponent4Arr = ...;
+     * int red255 = argbComponent4Arr[PArgbPixelMask.RED.component4ArrayIndex];
+     * }</pre>
+     * 
+     * @see #component3ArrayIndex
+     */
+    public final int component4ArrayIndex;
+    
+    /**
+     * Index for RGB (3-part) component arrays: {@link #component4ArrayIndex} minus one.  This
+     * value is invalid for {@link #ALPHA}.
+     * 
+     * @see #component4ArrayIndex
+     */
+    public final int component3ArrayIndex;
+    
+    private PArgbPixelMask(int bitShift, int component4ArrayIndex) {
         this.mask = 0xff << bitShift;
+        this.bitShift = bitShift;
+        this.component4ArrayIndex = component4ArrayIndex;
+        this.component3ArrayIndex = component4ArrayIndex - 1;
+    }
+    
+    /**
+     * This is a convenience method to call {@link #decomposeArgbPixelUnchecked(int, int[])},
+     * except inputs are carefully checked.  If speed is critical, or the inputs are well-sourced,
+     * consider directly calling {@link #decomposeArgbPixelUnchecked(int, int[])}.
+     * 
+     * @throws NullPointerException
+     *         if {@code outputArgbComponent4Arr} is {@code null}
+     * @throws IllegalArgumentException
+     * <ul>
+     *   <li>if pixel is outside range 0 to 255 (inclusive)</li>
+     *   <li>if number of elements in {@code outputArgbComponent4Arr} is not four (4)</li>
+     * </ul>
+     */
+    public static int[] decomposeArgbPixelChecked(int pixel, int[] outputArgbComponent4Arr) {
+        IntArgs.checkValueRange(pixel, 0, 255, "pixel");
+        // TODO: Change to use IntArrays.checkExactLength(...)
+        ArrayArgs.checkExactLength(outputArgbComponent4Arr, 4, "outputArgbComponent4Arr");
+        
+        int[] x = decomposeArgbPixelUnchecked(pixel, outputArgbComponent4Arr);
+        return x;
+    }
+    
+    /**
+     * Decomposes a single, integer-packed ARGB (or strictly RGB) pixel into its components: alpha,
+     * red, green, blue.  Each component has range 0 to 255.  If speed is not critical, or the
+     * inputs are poorly-sourced, consider using {@link #decomposeArgbPixelChecked(int, int[])}.
+     * If the resulting decomposition must be floats, use
+     * {@link #decomposeArgbPixelUnchecked(int, float[])}.
+     * 
+     * @param pixel
+     *        single, integer-packed ARGB (or strictly RGB) pixel
+     * @param outputArgbComponent4Arr
+     *        array with exact length four (4).  Must be pre-allocated
+     * 
+     * @return reference to decomposed array, {@code outputArgbComponent4Arr}
+     * 
+     * @see #decomposeArgbPixelChecked(int, int[])
+     * @see #decomposeArgbPixelUnchecked(int, float[])
+     * @see #component4ArrayIndex
+     */
+    public static int[] decomposeArgbPixelUnchecked(int pixel, int[] outputArgbComponent4Arr) {
+        for (PArgbPixelMask pixelMask: PArgbPixelMask.values()) {
+            outputArgbComponent4Arr[pixelMask.component4ArrayIndex] =
+                (pixel & pixelMask.mask) >> pixelMask.bitShift;
+        }
+        return outputArgbComponent4Arr;
+    }
+    
+    /**
+     * This is a convenience method to call {@link #decomposeArgbPixelUnchecked(int, float[])},
+     * except inputs are carefully checked.  If speed is critical, or the inputs are well-sourced,
+     * consider directly calling {@link #decomposeArgbPixelUnchecked(int, float[])}.
+     * 
+     * @throws NullPointerException
+     *         if {@code outputArgbComponent4Arr} is {@code null}
+     * @throws IllegalArgumentException
+     * <ul>
+     *   <li>if pixel is outside range 0 to 255 (inclusive)</li>
+     *   <li>if number of elements in {@code outputArgbComponent4Arr} is not four (4)</li>
+     * </ul>
+     */
+    public static float[] decomposeArgbPixelChecked(int pixel, float[] outputArgbComponent4Arr) {
+        IntArgs.checkValueRange(pixel, 0, 255, "pixel");
+        ArrayArgs.checkExactLength(outputArgbComponent4Arr, 4, "outputArgbComponent4Arr");
+        
+        float[] x = decomposeArgbPixelUnchecked(pixel, outputArgbComponent4Arr);
+        return x;
+    }
+    
+    /**
+     * Decomposes a single, integer-packed ARGB (or strictly RGB) pixel into its components: alpha,
+     * red, green, blue.  Each component has range 0.0f to 1.0f.  If speed is not critical, or the
+     * inputs are poorly-sourced, consider using {@link #decomposeArgbPixelChecked(int, float[])}.
+     * If the resulting decomposition must be ints, use
+     * {@link #decomposeArgbPixelUnchecked(int, int[])}.
+     * 
+     * @param pixel
+     *        single, integer-packed ARGB (or strictly RGB) pixel
+     * @param outputArgbComponent4Arr
+     *        array with exact length four (4).  Must be pre-allocated
+     * 
+     * @return reference to decomposed array, {@code outputArgbComponent4Arr}
+     * 
+     * @see #decomposeArgbPixelChecked(int, float[])
+     * @see #decomposeArgbPixelUnchecked(int, int[])
+     * @see #component4ArrayIndex
+     */
+    public static float[] decomposeArgbPixelUnchecked(int pixel, float[] outputArgbComponent4Arr) {
+        for (PArgbPixelMask pixelMask: PArgbPixelMask.values()) {
+            outputArgbComponent4Arr[pixelMask.component4ArrayIndex] =
+                ((pixel & pixelMask.mask) >> pixelMask.bitShift) / 255.0f;
+        }
+        return outputArgbComponent4Arr;
+    }
+    
+    /**
+     * This is a convenience method to call {@link #composeArgbPixelUnchecked(int[])}, except
+     * inputs are carefully checked.  If speed is critical, or the inputs are well-sourced,
+     * consider directly calling {@link #composeArgbPixelUnchecked(int[])}.
+     * 
+     * @throws NullPointerException
+     *         if {@code outputArgbComponent4Arr} is {@code null}
+     * @throws IllegalArgumentException
+     *         if number of elements in {@code outputArgbComponent4Arr} is not four (4)
+     */
+    public static int composeArgbPixelChecked(int[] argbComponent4Arr) {
+        ArrayArgs.checkExactLength(argbComponent4Arr, 4, "argbComponent4Arr");
+        
+        int x = composeArgbPixelUnchecked(argbComponent4Arr);
+        return x;
+    }
+    
+    /**
+     * Composes a single, integer-packed ARGB pixel from its components: alpha, red, green, blue.
+     * Each component has range 0 to 255.  If speed is not critical, or the
+     * inputs are poorly-sourced, consider using {@link #composeArgbPixelChecked(int[])}.  If
+     * composing floats, use {@link #composeArgbPixelUnchecked(float[])}.
+     * 
+     * @param argbComponent4Arr
+     *        array with exact length four (4) with ARGB components
+     * 
+     * @return composed ARGB pixel
+     * 
+     * @see #composeArgbPixelChecked(int[])
+     * @see #composeArgbPixelUnchecked(float[])
+     */
+    public static int composeArgbPixelUnchecked(int[] argbComponent4Arr) {
+        int pixel = 0;
+        for (PArgbPixelMask pixelMask: PArgbPixelMask.values()) {
+            pixel |= (argbComponent4Arr[pixelMask.component4ArrayIndex] << pixelMask.bitShift);
+        }
+        return pixel;
+    }
+    
+    /**
+     * This is a convenience method to call {@link #composeArgbPixelUnchecked(float[])}, except
+     * inputs are carefully checked.  If speed is critical, or the inputs are well-sourced,
+     * consider directly calling {@link #composeArgbPixelUnchecked(float[])}.
+     * 
+     * @throws NullPointerException
+     *         if {@code outputArgbComponent4Arr} is {@code null}
+     * @throws IllegalArgumentException
+     * <ul>
+     *   <li>if pixel is outside range 0.0f to 1.0f (inclusive)</li>
+     *   <li>if number of elements in {@code outputArgbComponent4Arr} is not four (4)</li>
+     * </ul>
+     */
+    public static int composeArgbPixelChecked(float[] argbComponent4Arr) {
+        ArrayArgs.checkExactLength(argbComponent4Arr, 4, "argbComponent4Arr");
+        
+        int x = composeArgbPixelUnchecked(argbComponent4Arr);
+        return x;
+    }
+    
+    /**
+     * Composes a single, integer-packed ARGB pixel from its components: alpha, red, green, blue.
+     * Each component has range 0.0f to 1.0f.  If speed is not critical, or the
+     * inputs are poorly-sourced, consider using {@link #composeArgbPixelChecked(float[])}.  If
+     * composing ints, use {@link #composeArgbPixelUnchecked(int[])}.
+     * 
+     * @param argbComponent4Arr
+     *        array with exact length four (4) with ARGB components
+     * 
+     * @return composed ARGB pixel
+     * 
+     * @see #composeArgbPixelChecked(float[])
+     * @see #composeArgbPixelUnchecked(int[])
+     */
+    public static int composeArgbPixelUnchecked(float[] argbComponent4Arr) {
+        int pixel = 0;
+        for (PArgbPixelMask pixelMask: PArgbPixelMask.values()) {
+            int scaled = (int) (255.0f * argbComponent4Arr[pixelMask.component4ArrayIndex]);
+            pixel |= (scaled << pixelMask.bitShift);
+        }
+        return pixel;
     }
 }
